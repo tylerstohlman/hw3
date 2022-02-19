e22c0c22f00aafc7321a49d04dec6ba79da80801
tylerstohlman <tyler.stohlman@kellogg.northwestern.edu>
2022-02-19 00:39:11 +0000

    6:30 update 2.18

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,37 @@
 class PlacesController < ApplicationController
+
+    def index
+        @places = Place.all
+      end
+    
+      def show
+        @place = Place.find(params["id"])
+      end
+    
+      def new
+        @place = Place.new
+      end
+    
+      def create
+        @place = Place.new(params["place"])
+        @place.save
+        redirect_to "/places"
+      end
+    
+      def edit
+        @place = Place.find(params["id"])
+      end
+    
+      def update
+        @place = Place.find(params["id"])
+        @place.update(params["place"])
+        redirect_to "/places"
+      end
+    
+      def destroy
+        @place = Place.find(params["id"])
+        @place.destroy
+        redirect_to "/places"
+      end
+
 end
